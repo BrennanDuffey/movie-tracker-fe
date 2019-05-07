7bf0d322143e0bc0909d14f836ccb145eee482c1
SiimonStark <justinpyktel@gmail.com>
2019-05-07 16:24:16 -0600

    Setup Reducer files
    Co-authored-by: BrennanDeffey <brennanduffey@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {Provider} from 'react-redux';
+import rootReducer from './reducers/';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store =createStore(rootReducer, composeWithDevTools());
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </ Provider>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
