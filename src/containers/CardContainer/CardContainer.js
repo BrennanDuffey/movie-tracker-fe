d2e9b464ba543a6cf565dc1111a6a0a553fb158b
SiimonStark <justinpyktel@gmail.com>
2019-05-07 22:58:16 -0600

    rough base for our styling

--- src/containers/CardContainer/CardContainer.js
+++ src/containers/CardContainer/CardContainer.js
@@ -16,18 +16,20 @@ class CardContainer extends Component {
         <div className="image">
         </div>
         <div className="content">
-          <h3>{this.props.latest.title}</h3>
-          <p>{this.props.latest.genres}</p>
-          <p>{this.props.latest.tagline}</p>
+          <h3>this.props.latest.title</h3>
+          <p>this.props.latest.genres</p>
+          <p>this.props.latest.tagline</p>
         </div>
         <div className="rating">
-          <h4>{this.props.latest.rating}</h4>
+          <h4>3.7</h4>
         </div>
       </article>;
 
     return (
       <main className="CardContainer">
-        <header>Search</header>
+        <header>
+          <div className="search-icon"></div>
+        </header>
         <section className="cards-display">
           {displayCard}
         </section>
