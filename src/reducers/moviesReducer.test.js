163f8e55f26cc250540e3074a3b9d50d592e6f3e
SiimonStark <justinpyktel@gmail.com>
2019-05-09 10:21:40 -0600

    Verifying Existing User
    Co-authored-by: BrennanDeffey <brennanduffey@gmail.com>

--- src/reducers/moviesReducer.test.js
+++ src/reducers/moviesReducer.test.js
@@ -1,1 +1,33 @@
 import moviesReducer from './moviesReducer';
+import * as actions from './';
+
+describe('moviesReducer', ()=>{
+
+  it('should return the initial state', ()=>{
+    //Setup
+    const expected = [];
+    //Execution
+    const result = moviesReducer(undefined, {});
+    //Expectation
+    expect(result).toEqual(expected);
+  });
+
+  it('should return an action object with a an array of movies and type of FETCH_MOVIES', ()=>{
+    //Setup
+    const movies = [{
+      title: 'Harry Potter',
+      release: 2006, 
+      poster: 'fds35235/',
+      backdrop: '3425/',
+      rating: 3.7,
+      summary: 'Youre a wizard harry',
+      id: 2,
+      genres: ['scifi', 'adventure']
+    }];
+    const expected = {type: 'FETCH_MOVIES', movies};
+    //Execution
+    const result = actions.addMovies(movies);
+    //Expectation
+    expect(result).toEqual(expected);
+  })
+})
