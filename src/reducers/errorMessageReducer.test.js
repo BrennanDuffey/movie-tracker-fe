27aeaae6e00227b758517e7adfb9880ec721ec25
Brennan <brennanduffey@gmail.com>
2019-05-14 19:42:22 -0600

    Added currMovieReducer test

---
+++ src/reducers/errorMessageReducer.test.js
@@ -0,0 +1,6 @@
+import errorMessageReducer from './errorMessageReducer';
+import * as actions from '../actions';
+
+describe('errorMessageReducer', () => {
+  it('Should return the default stae')
+})
