a0585415a88ab23f3340846eb48eac805b517845
Brennan <brennanduffey@gmail.com>
2019-05-15 08:16:41 -0600

    Updates

---
+++ precache-manifest.431561daea25f1ffa175a8fc867b412e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c60e09f4e4c6f3f8e062a7f02251a6",
+    "url": "/butterflix/index.html"
+  },
+  {
+    "revision": "68e58ff6199d5a52cbf3",
+    "url": "/butterflix/static/css/main.dc101346.chunk.css"
+  },
+  {
+    "revision": "e9ebb56e439f8136163f",
+    "url": "/butterflix/static/js/2.35235ba7.chunk.js"
+  },
+  {
+    "revision": "68e58ff6199d5a52cbf3",
+    "url": "/butterflix/static/js/main.f5b2bc88.chunk.js"
+  },
+  {
+    "revision": "3f7922dea79ba29b9cfd",
+    "url": "/butterflix/static/js/runtime~main.ee4ff867.js"
+  },
+  {
+    "revision": "054c9f72159d712cdca2e557ce1a5b5d",
+    "url": "/butterflix/static/media/character.054c9f72.svg"
+  }
+]);
