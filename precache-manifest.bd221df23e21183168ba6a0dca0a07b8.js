f5f69ae40417de1a143cfb1c149cf472974c9b63
Brennan <brennanduffey@gmail.com>
2019-05-15 08:22:02 -0600

    Updates

---
+++ precache-manifest.bd221df23e21183168ba6a0dca0a07b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0461e54593b9e3e6ca56fa49624fc709",
+    "url": "/movie-tracker-fe/index.html"
+  },
+  {
+    "revision": "47f65df6748d225e9e9e",
+    "url": "/movie-tracker-fe/static/css/main.dc101346.chunk.css"
+  },
+  {
+    "revision": "e9ebb56e439f8136163f",
+    "url": "/movie-tracker-fe/static/js/2.35235ba7.chunk.js"
+  },
+  {
+    "revision": "47f65df6748d225e9e9e",
+    "url": "/movie-tracker-fe/static/js/main.29957733.chunk.js"
+  },
+  {
+    "revision": "f609b1748efe3f07d574",
+    "url": "/movie-tracker-fe/static/js/runtime~main.fb3e4a12.js"
+  },
+  {
+    "revision": "054c9f72159d712cdca2e557ce1a5b5d",
+    "url": "/movie-tracker-fe/static/media/character.054c9f72.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/butterflix/precache-manifest.431561daea25f1ffa175a8fc867b412e.js"
+  "/movie-tracker-fe/precache-manifest.bd221df23e21183168ba6a0dca0a07b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/butterflix/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie-tracker-fe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
